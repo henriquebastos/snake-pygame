df5d603ab4d6648a6bc78a2b943e9dd265ac8f8a
Henrique Bastos <henrique@bastos.net>
2019-11-21 00:51:46 -0300

    Mudei a porra toda na empolgação :D

--- snake/__main__.py
+++ snake/__main__.py
@@ -1,43 +1,37 @@
+from random import randrange
+
 import pygame
-import random
 from pygame.locals import QUIT, KEYDOWN, K_UP, K_DOWN, K_RIGHT, K_LEFT
 from sys import exit
 
 from snake.characters import Snake, Directions, Point, Apple
 
 
-def posicao_aleatoria():
-    x = random.randint(0, 630)
-    y = random.randint(0, 470)
-    return (x // 10 * 10, y // 10 * 10)
-
-def colisao(objetoA, objetoB):
-    return (objetoA[0] == objetoB[0]) and (objetoA[1] == objetoB[1])
+def random_point():
+    return Point(randrange(63), randrange(47))
 
 
-# Definindo a nossa cobra
-snake_pos = Snake(Point(30, 30))
+snake = Snake(Point(30, 30))
+apple = Apple(*random_point())
+direcao = Directions.LEFT
 
-# Definindo a nossa maçã
-apple_position = posicao_aleatoria()
-
-direcao = Directions.LEFT # Esquerda
-
+WIDTH, HEIGHT = 64, 48
+SCALE = 10
 
 def main():
-    global snake_pos, apple_position, direcao
+    global snake, apple, direcao
 
     running = True
 
     pygame.init()
 
-    screen = pygame.display.set_mode((640, 480), 0, 32)
-    pygame.display.set_caption("Snake em Python com Pygame")
+    screen = pygame.display.set_mode((WIDTH * SCALE, HEIGHT * SCALE), 0, 32)
+    pygame.display.set_caption("Stallone Cobra")
 
     clock = pygame.time.Clock()
 
     while running:
-        clock.tick(10)
+        clock.tick(10)  #TODO: Usar um Tick que não bloquei a execução.
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -54,17 +48,17 @@ def main():
                 if event.key == K_LEFT:
                     direcao = Directions.LEFT
 
-        if colisao(snake_pos[0] * 10, apple_position):
-            apple_position = posicao_aleatoria()
-            snake_pos.eat()
+        if snake[0] == apple:
+            apple = Apple(*random_point())
+            snake.eat()
 
-        snake_pos.turn(direcao)
-        snake_pos.slither()
+        snake.turn(direcao)
+        snake.slither()
 
         screen.fill((0, 0, 0))
-        screen.fill((255, 0, 0), (*apple_position, 10, 10))
-        for p in snake_pos:
-            screen.fill((0, 255, 0), (p.x * 10, p.y * 10, 10, 10))
+        screen.fill((255, 0, 0), (*(apple * SCALE), SCALE, SCALE))
+        for p in snake:
+            screen.fill((0, 255, 0), (*(p * SCALE), SCALE, SCALE))
 
         pygame.display.update()
 
